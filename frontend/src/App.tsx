b60c6a57bd4e93cfe9c8a7f0886a7e98768b1b9e
showtim3 <coolrishabhrawat@gmail.com>
2021-04-02 18:21:25 +0530

    Fixing global css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -15,21 +15,22 @@ import GlobalCss from "./components/home/styles/jss/GlobalCss";
 function App() {
   return (
     <MuiThemeProvider theme={Theme}>
-      <GlobalCss/>
-      <div className={"App"}>
-        <Router>
-          <div className={"app-content"}>
-            <Switch>
-              <Route path='/' exact={true} component={Home}/>
-              <Route path='/login' exact={true} component={Login}/>
-              <Route path='/register' exact={true} component={Register}/>
-              <Route path= '/posts' exact={true} component={Post}/>
-              <Route path= '/account' exact={true} component={Account}/>
-              <Route path= '/dashboard' exact={true} component={DashBoard}/>
-            </Switch>
-          </div>
-        </Router>
-      </div>
+      <GlobalCss>
+        <div className={"App"}>
+          <Router>
+            <div className={"app-content"}>
+              <Switch>
+                <Route path='/' exact={true} component={Home}/>
+                <Route path='/login' exact={true} component={Login}/>
+                <Route path='/register' exact={true} component={Register}/>
+                <Route path= '/posts' exact={true} component={Post}/>
+                <Route path= '/account' exact={true} component={Account}/>
+                <Route path= '/dashboard' exact={true} component={DashBoard}/>
+              </Switch>
+            </div>
+          </Router>
+        </div>
+      </GlobalCss>
     </MuiThemeProvider>
   );
 }
